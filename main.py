a43db7c4e5ba383235e76778f1a0e7cb9ccf9b4d
Ferrarisrex <91031616+Ferrarisrex@users.noreply.github.com>
2023-05-18 11:12:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ def scrape_website(url):
     
     # Example: Extract all the links on the webpage
     links = soup.find_all('a')
-    for link in links:
-        print(link['href'])
+    links_data = [link['href'] for link in links]
     
     # Example: Extract all the headings on the webpage
     headings = soup.find_all(['h1', 'h2', 'h3'])
-    for heading in headings:
-        print(heading.text.strip())
+    headings_data = [heading.text.strip() for heading in headings]
+    
+    # Save the scraped data to a text file
+    with open(f"{url.split('//')[1]}.txt", "w") as file:
+        file.write("Links:\n")
+        file.write("\n".join(links_data))
+        file.write("\n\nHeadings:\n")
+        file.write("\n".join(headings_data))
 
 # Get the website URL from the user
 website_url = input("Enter the website URL: ")
